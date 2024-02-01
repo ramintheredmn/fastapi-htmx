2aa6d9f7ff5df0379ba0411e0ead8de511e1bb33
ramintheredmn <redcrossprog@gmail.com>
2024-02-01 13:54:27 +0330

    Revert " major changes"
    This reverts commit 18b53f3f6e71e484ee0987054014baaa6300cf7a.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import pandas as pd
-
 # define the app, an instance of the Fastapi() class
 app = FastAPI()
 # mount the static folder to be used by fastapi
@@ -14,89 +13,39 @@ templates = Jinja2Templates(directory="app/templates")
 # read the csv of medicines to access in different routs
 df = pd.read_csv(r"./app/medicine.csv")
 drugs_dict = {}
-
-
 # define the root route, that returns the index.html
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-
-# receive post reqs from mobile app
-
-
-@app.post("/api/receive")
-async def receive(request: Request):
-    if request:
-        try:
-            data = await request.json()
-            print(data)
-        except Exception as e:
-            print(e)
-            return f"error: {str(e)}"
-    else:
-        print("no data")
-    return "received"
-
-
 @app.post("/api/drug_search", response_class=HTMLResponse)
 async def drug_search(request: Request, query: str = Form(...)):
     filtered_df: Any = df[df["Drug Name"].str.contains(query, case=False)]
-    drugs = filtered_df["Drug Name"].drop_duplicates().tolist()
-    return templates.TemplateResponse(
-        "search.html", {"request": request, "options": drugs}
-    )
-
+    drugs = filtered_df['Drug Name'].drop_duplicates().tolist()
+    return templates.TemplateResponse("search.html", {"request": request, "options": drugs})
 
 @app.post("/api/search/drugname", response_class=HTMLResponse)
 async def drugname(request: Request, drugname: str = Form(...)):
-    drugname_filtered_df: Any = df[df["Drug Name"] == str(drugname)]
-    salts = drugname_filtered_df["Salt"].drop_duplicates().tolist()
-    dosageforms = drugname_filtered_df["Dosage Form"].drop_duplicates().tolist()
-    strengths = drugname_filtered_df["Strengh"].drop_duplicates().tolist()
-    roas = drugname_filtered_df["Route of Admin"].drop_duplicates().tolist()
-    list_data = {
-        "salt": salts,
-        "dosageform": dosageforms,
-        "strength": strengths,
-        "roa": roas,
-    }
+    drugname_filtered_df: Any = df[df['Drug Name'] == str(drugname)]
+    salts = drugname_filtered_df['Salt'].drop_duplicates().tolist()
+    dosageforms = drugname_filtered_df['Dosage Form'].drop_duplicates().tolist()
+    strengths = drugname_filtered_df['Strengh'].drop_duplicates().tolist()
+    roas = drugname_filtered_df['Route of Admin'].drop_duplicates().tolist()
+    list_data = {'salt': salts,'dosageform': dosageforms, 'strength': strengths, 'roa': roas}
     for key, value in list_data.items():
         if str(value[0]) == "nan":
-            value[0] = f"No particular {key} for this drug"
-    return templates.TemplateResponse(
-        "drugInfo.html",
-        {
-            "request": request,
-            "drugname": drugname,
-            "salts": salts,
-            "dosageforms": dosageforms,
-            "strengths": strengths,
-            "roas": roas,
-        },
-    )
-
-
+            value[0] = (f"No particular {key} for this drug")
+    return templates.TemplateResponse("drugInfo.html", {"request": request, 'drugname': drugname, "salts": salts, "dosageforms": dosageforms, "strengths": strengths, "roas": roas})
 @app.post("/api/search/drugname/druginfo", response_class=HTMLResponse)
-async def drugname_salt(
-    request: Request,
-    drugname: str = Form(...),
-    salt: str = Form(...),
-    dosageform: str = Form(...),
-    strength: str = Form(...),
-    roa: str = Form(...),
-):
+async def drugname_salt(request: Request, drugname:str = Form(...), salt: str = Form(...), dosageform: str = Form(...), strength: str = Form(...), roa: str = Form(...)):
     last = ""
-    if "No particular" not in salt:
-        last += f", {salt}"
-    if "No particular" not in dosageform:
-        last += f", {dosageform}"
-    if "No particular" not in strength:
-        last += f", {strength}"
-    if "No particular" not in roa:
-        last += f", {roa}"
+    if 'No particular' not in salt:
+        last += f', {salt}'
+    if 'No particular' not in dosageform:
+        last += f', {dosageform}'
+    if 'No particular' not in strength:
+        last += f', {strength}'
+    if 'No particular' not in roa:
+        last += f', {roa}'
     drugs_dict[drugname] = last
     total = drugs_dict
-    return templates.TemplateResponse(
-        "druglist.html", {"request": request, "last": drugname + last, "total": total}
-    )
+    return templates.TemplateResponse("druglist.html", {'request': request, 'last': drugname + last , 'total': total})
