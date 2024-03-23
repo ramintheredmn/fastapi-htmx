dd9e8234e2f37e869258da01406551a737502257
ramintheredmn <redcrossprog@gmail.com>
2024-03-24 02:44:42 +0330

    tailwind fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/templates/**/*.html", "./app/**/*.py"],
+  content: ["./app/templates/**/*.html"],
   theme: {
     extend: {},
   },
